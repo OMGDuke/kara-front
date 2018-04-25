04a4cf818a33ea3a0dc1233eb44dfaea2d2b783d
OMGDuke <alex@omgduke.com>
2018-04-25 19:34:08 +0900

    Header queue now shows the next song rather than the current song@

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -11,59 +11,64 @@ class Header extends Component {
     return `${new Date().getTime()}_${Math.random()}`;
   }
   showRoom() {
-    if(this.props.queue) {
-      return <h3>Room: {this.props.room}</h3>
+    if (this.props.queue) {
+      return <h3>Room: {this.props.room}</h3>;
     }
   }
 
   goToQueue() {
-    this.props.history.push('/queue')
+    this.props.history.push('/queue');
   }
   goHome() {
     if (this.props.joined && this.props.role === 'server') {
-      this.props.history.push('/sing')
+      this.props.history.push('/sing');
     } else if (this.props.joined && this.props.role === 'user') {
-      this.props.history.push('/search')
-    };
-    
+      this.props.history.push('/search');
+    }
   }
   showQueue() {
-    if(this.props.queue) {
-      return(
+    if (this.props.queue) {
+      return (
         <div>
           <h2>Queue: {this.props.queue.length} Songs</h2>
           <h3>Up Next:</h3>
-          {this.props.queue.slice(0,1).map(song => <p key={this.generateKey()}>{song.snippet.title}</p>)}
-          <button onClick={this.goToQueue}><h5>Full Queue</h5></button>
-          <button className="float-right" onClick={this.props.nextSong}>Skip Song</button>
+          {this.props.queue
+            .slice(1, 2)
+            .map(song => <p key={this.generateKey()}>{song.snippet.title}</p>)}
+          <button onClick={this.goToQueue}>
+            <h5>Full Queue</h5>
+          </button>
+          <button className="float-right" onClick={this.props.nextSong}>
+            Skip Song
+          </button>
         </div>
-      )
+      );
     }
   }
   render() {
     return (
       <div className="Header">
         <div className="jumbotron jumbotron-fluid">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-4 order-md-2">
-                  <a href="#" onClick={this.goHome}><h1 className="display-4">{process.env.REACT_APP_WEBSITE_NAME}</h1></a>
-                  <p className="lead">Witty Tagline</p>
-                </div>
-                <div className="col-md-4 order-md-1">
-                {this.showRoom()}
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4 order-md-2">
+                <a href="#" onClick={this.goHome}>
+                  <h1 className="display-4">
+                    {process.env.REACT_APP_WEBSITE_NAME}
+                  </h1>
+                </a>
+                <p className="lead">Witty Tagline</p>
               </div>
-                <div className="col-md-4 order-md-3">
-                  {this.showQueue()}
-                </div>
-              </div>
+              <div className="col-md-4 order-md-1">{this.showRoom()}</div>
+              <div className="col-md-4 order-md-3">{this.showQueue()}</div>
             </div>
+          </div>
         </div>
       </div>
-    );  
+    );
   }
 }
 
-const RouterHeader = withRouter(Header)
+const RouterHeader = withRouter(Header);
 
 export default RouterHeader;
